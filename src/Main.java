ee671e227e0b05bf602ebc99735a1be8aede1260
Michal_Matejczuk <01171250@pw.edu.pl>
2023-01-01 23:51:19 +0100

    fixes

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-//import javafx.application.Application;
-//import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 
-//public class Main extends Application {
-//
-//    public static void main(String[] args) {
-//
-//        launch(args);
-//    }
-//
-//    public void start(Stage stage) throws Exception{
-//
-//        stage.show();
-//    }
-//}
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception{
+
+        stage.show();
+    }
+}
