35ac9effe5c436f8d20935c2a14fb8bff1041d31
Michal_Matejczuk <01171250@pw.edu.pl>
2023-01-23 13:34:57 +0100

    dodano setName do Member

--- src/backend/Member.java
+++ src/backend/Member.java
@@ -11,6 +11,10 @@ public class Member {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "backend.Member{" +
